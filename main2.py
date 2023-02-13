7cfa488f6fd60e35e36c52d7431eb66a7917adff
Flike <40733747+FlikeID@users.noreply.github.com>
2023-02-13 23:51:39 +0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -253,7 +253,7 @@ def admin_code(database, code, chat_id, user_id, name=False):
 
 with open('database.json', 'r', encoding='utf-8') as f:  # открыли файл с данными
     database = json.load(f)  # загнали все, что получилось в переменную
-#BOT = EvgenyBot('5169189756:AAG83jWE7euED8fABalO_aky5HZJXiW5D0k', DEBUG)
+    
 BOT = EvgenyBot(TOKEN, DEBUG)
 Actions = Actions()
 
